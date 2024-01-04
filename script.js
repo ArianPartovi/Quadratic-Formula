7be8f2a79113facc2c52411328fb39d8c835152e
Arian Partovi <arirayan42@gmail.com>
2024-01-03 17:16:18 -0800

    added comment

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function calculate() {
-    var a = document.getElementById('a').value;
-    var b = document.getElementById('b').value;
-    var c = document.getElementById('c').value;
+    var a = document.getElementById('a').value; //declaring variable a
+    var b = document.getElementById('b').value; //declaring variable b
+    var c = document.getElementById('c').value; //declaring variable c
 
-    var discriminant = b * b - 4 * a * c;
-    var root1, root2;
+    var discriminant = b * b - 4 * a * c; //inside of the square root formula
+    var root1, root2; // answers are root1 and root 2
     var resultText = '';
 
-    if (discriminant > 0) {
+    if (discriminant > 0) { //scenario if the inside of the square root is a value greater than 0
         root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
         root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         resultText = `Roots are real and different<br>Root 1 = ${root1.toFixed(2)}<br>Root 2 = ${root2.toFixed(2)}`;
-    } else if (discriminant == 0) {
+    } else if (discriminant == 0) { //scenario if the inside of the square root is a value equal to 0
         root1 = root2 = -b / (2 * a);
         resultText = `Roots are real and same<br>Root = ${root1.toFixed(2)}`;
-    } else {
+    } else { //scenario if the the result is none of the above
         var realPart = (-b / (2 * a)).toFixed(2);
         var imaginaryPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
         resultText = `Roots are complex<br>Root 1 = ${realPart} + ${imaginaryPart}i<br>Root 2 = ${realPart} - ${imaginaryPart}i`;
